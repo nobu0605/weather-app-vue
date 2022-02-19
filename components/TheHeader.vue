48a09ece4a5ca52c13f335cd1f68d37772de28bb
nobu0605 <nobublack555@gmail.com>
2022-02-19 17:38:20 +0900

    initial commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      title: 'Hello, World!',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'description' },
+      ],
+    }
+  },
+}
+</script>
